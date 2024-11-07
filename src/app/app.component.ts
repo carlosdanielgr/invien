0374364200afc01a1cc3a6d07b37ec72de65cfee
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-07 12:43:49 -0500

    contact component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
