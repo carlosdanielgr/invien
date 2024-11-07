f6f7b0e7eb1e1f4961d26848a563c6bbf4f7151c
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-07 13:09:40 -0500

    header scrolled completed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
+    if (scrollPosition > 50) {
+      this.addClass();
+    } else this.removeClass();
+  }
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  addClass(): void {
+    this.renderer.addClass(
+      this.el.nativeElement.querySelector('header'),
+      'navbar-scrolled'
+    );
+  }
+
+  removeClass(): void {
+    this.renderer.removeClass(
+      this.el.nativeElement.querySelector('header'),
+      'navbar-scrolled'
+    );
+  }
 }
