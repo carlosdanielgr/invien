db07edd8215d6de24f66025f0a5397799c6cc1ca
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-07 17:57:20 -0500

    home-page component created, pending sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -7,7 +8,7 @@ import { ContactComponent } from './components/contact/contact.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, ContactComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
