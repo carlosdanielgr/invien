eb0aa894eace5d0e0524577f629dcbab75df58ae
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-07 11:32:34 -0500

    Project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'invien';
-}
+export class AppComponent {}
