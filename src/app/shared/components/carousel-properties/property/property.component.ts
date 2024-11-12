b5247c407236ad7c978ce53ac884cfeb793570a2
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-12 12:30:51 -0500

    partial changes to property detail component

--- src/app/shared/components/carousel-properties/property/property.component.ts
+++ src/app/shared/components/carousel-properties/property/property.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Property } from '@shared/interfaces/property.interface';
 
@@ -13,4 +14,10 @@ export class PropertyComponent {
   @Input() property: Partial<Property> = {};
 
   @Input() info: string = '';
+
+  constructor(private readonly router: Router) {}
+
+  redirectProperty() {
+    this.router.navigate(['/properties', 1]);
+  }
 }
