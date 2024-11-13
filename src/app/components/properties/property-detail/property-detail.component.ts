933b1b82031f90aa80ff78e20d0a7e61af60948a
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-13 12:19:48 -0500

    property detail partially completed

--- src/app/components/properties/property-detail/property-detail.component.ts
+++ src/app/components/properties/property-detail/property-detail.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
+import { CarouselPropertiesComponent } from '@shared/components/carousel-properties/carousel-properties.component';
+import { FormComponent } from '../../form/form.component';
+
 @Component({
   selector: 'app-property-detail',
   standalone: true,
-  imports: [],
+  imports: [FormComponent, CarouselPropertiesComponent],
   templateUrl: './property-detail.component.html',
-  styleUrl: './property-detail.component.scss'
+  styleUrl: './property-detail.component.scss',
 })
-export class PropertyDetailComponent {
-
-}
+export class PropertyDetailComponent {}
