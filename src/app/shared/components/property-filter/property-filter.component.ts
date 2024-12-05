5ac4679db192eda890c557ce68e2d287cf429697
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-12-04 23:20:44 -0500

    fix translations and redirect completed

--- src/app/shared/components/property-filter/property-filter.component.ts
+++ src/app/shared/components/property-filter/property-filter.component.ts
@@ -71,10 +71,55 @@ export class PropertyFilterComponent implements OnInit {
     this.currentFiltersName[key] = name;
   }
 
-  onSetFilterRange(key: string, min: string, max: string, name: string) {
+  onSetFilterRange(key: string, min: string, max: string) {
     this.currentFilters[`max${key}`] = max;
     this.currentFilters[`min${key}`] = min;
-    this.currentFiltersName[key] = name;
+    let value = '';
+    if (!min && max) {
+      if (key === 'Price')
+        value = $localize`:@@prop-filter-less:Menos de` + ' $' + max;
+      else if (key === 'Rooms')
+        value = $localize`:@@prop-filter-less:Menos de` + ' ' + max;
+      else if (key === 'TotalSize')
+        value = $localize`:@@prop-filter-less:Menos de` + ' ' + max + 'm2';
+    } else if (min && !max) {
+      if (key === 'Price')
+        value = $localize`:@@prop-filter-more:Más de` + ' $' + min;
+      else if (key === 'Rooms')
+        value = $localize`:@@prop-filter-more:Más de` + ' ' + min;
+      else if (key === 'TotalSize')
+        value = $localize`:@@prop-filter-more:Más de` + ' ' + min + 'm2';
+    } else if (min && max) {
+      if (key === 'Price')
+        value =
+          $localize`:@@between:Entre` +
+          ' $' +
+          min +
+          ' ' +
+          $localize`:@@and:y` +
+          ' $' +
+          max;
+      else if (key === 'Rooms')
+        value =
+          $localize`:@@between:Entre` +
+          ' ' +
+          min +
+          ' ' +
+          $localize`:@@and:y` +
+          ' ' +
+          max;
+      else if (key === 'TotalSize')
+        value =
+          $localize`:@@between:Entre` +
+          ' ' +
+          min +
+          'm2 ' +
+          $localize`:@@and:y` +
+          ' ' +
+          max +
+          'm2';
+    }
+    this.currentFiltersName[key] = value;
   }
 
   onSearch() {
